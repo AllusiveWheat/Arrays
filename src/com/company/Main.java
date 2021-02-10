f300ce5644b2739cb4fb190ae119e079b834fa9e
AllusiveWheat <37462003+AllusiveWheat@users.noreply.github.com>
2021-02-10 11:00:38 -0800

    Added Revered Order

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        int[] intArray  =  new int[] {69,62,43,5,6,123,5323,5774};
-        int  temp  = 0;
-        //Print original array
+        Integer[] IntArray = {69, 32323242, 425432452, 35435, 345345, 535435, 453535, 3232, 23424243, 1353436535};
+
+
+    /*    //Print original array
         System.out.println("Original array: ");
         for (int i = 0; i <intArray.length; i++) {
             System.out.print(intArray[i] + " ");
@@ -20,9 +25,22 @@ public class Main {
             }
         }
         //  Print Sorted Array
-        System.out.println("Array  sorted  in ascending order ");
+       System.out.println("Array  sorted  in ascending order ");
         for(int i = 0; i<intArray.length; i++){
-            System.out.print(intArray[i] + " ");
-        }
+            System.out.print(intArray[i] + " "); */
+
+        // Print Original Array using Arrays.sort method
+            System.out.printf("Original Array : %s" , Arrays.toString((IntArray)));
+        // Sorted Using Arrays.sort
+
+        Arrays.sort(IntArray);
+        System.out.printf("Sorted Array : %s" , Arrays.toString((IntArray)));
+
+        Arrays.sort(IntArray , Collections.reverseOrder());
+
+        System.out.printf(" Reversed Sort Array : %s" , Arrays.toString((IntArray)));
+
+
+
     }
-}
+    }
