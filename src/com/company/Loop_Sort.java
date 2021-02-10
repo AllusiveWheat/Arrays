14debb273656fba18a7c5f1f3ac7e1a9357f216e
AllusiveWheat <37462003+AllusiveWheat@users.noreply.github.com>
2021-02-10 11:09:37 -0800

    Updates

---
+++ src/com/company/Loop_Sort.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Loop_Sort {
+    public static void main(String[] args) {
+    int[] intArray = {69,67,454,23254,454543,23432424,11342343};
+    int temp = 0;
+
+    //Print original array
+        System.out.println("Original array: ");
+        for (int i = 0; i <intArray.length; i++) {
+            System.out.print(intArray[i] + " ");
+        }
+        // Sort  Array in  Ascending Order
+        for(int i  = 0; i <intArray.length; i++) {
+            for (int  j = i+1;  j< intArray.length; j++) {
+
+                if(intArray[i] > intArray[j]) {
+                    temp  = intArray[i];
+                    intArray[i]    = intArray[j];
+                    intArray[j]  =  temp;
+                }
+            }
+        }
+        //  Print Sorted Array
+       System.out.println("Array  sorted  in ascending order ");
+        for(int i = 0; i<intArray.length; i++){
+            System.out.print(intArray[i] + " ");
+
+
+        }
+    }
+}
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,41 +6,12 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
         Integer[] IntArray = {69, 32323242, 425432452, 35435, 345345, 535435, 453535, 3232, 23424243, 1353436535};
-
-
-    /*    //Print original array
-        System.out.println("Original array: ");
-        for (int i = 0; i <intArray.length; i++) {
-            System.out.print(intArray[i] + " ");
-        }
-        // Sort  Array in  Ascending Order
-        for(int i  = 0; i <intArray.length; i++) {
-            for (int  j = i+1;  j< intArray.length; j++) {
-
-                if(intArray[i] > intArray[j]) {
-                    temp  = intArray[i];
-                    intArray[i]    = intArray[j];
-                    intArray[j]  =  temp;
-                }
-            }
-        }
-        //  Print Sorted Array
-       System.out.println("Array  sorted  in ascending order ");
-        for(int i = 0; i<intArray.length; i++){
-            System.out.print(intArray[i] + " "); */
-
         // Print Original Array using Arrays.sort method
-            System.out.printf("Original Array : %s" , Arrays.toString((IntArray)));
+        System.out.printf("Original Array : %s" , Arrays.toString((IntArray)));
         // Sorted Using Arrays.sort
-
         Arrays.sort(IntArray);
         System.out.printf("Sorted Array : %s" , Arrays.toString((IntArray)));
-
         Arrays.sort(IntArray , Collections.reverseOrder());
-
         System.out.printf(" Reversed Sort Array : %s" , Arrays.toString((IntArray)));
-
-
-
     }
-    }
+}
