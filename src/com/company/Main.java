7e39bccfdeba01ab3dec54d28706e27f3837f05f
AllusiveWheat <37462003+AllusiveWheat@users.noreply.github.com>
2021-02-10 10:31:43 -0800

    Sorting Array in Ascending Order

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,28 @@
 package com.company;
-import java.util.Arrays;
 public class Main {
-
-    public static String[] studentNames(){
-   /*Returns an array of all of the first names of students in
-   this class, sorted alphabetically A->Z by first name
-   Inputs: none
-   Outputs:
-       return: a String[] with alphabetized student first names
-   Ex.
-   studentNames() -> [Alexa, Ana, Benton, Ethan,...]
-   */
-        String[] studentNames = new String[15];
-        studentNames[0] = "Alex";
-        studentNames[1] = "Ben";
-        studentNames[2] = "Charlie";
-        studentNames[3] = "Connor";
-        studentNames[4] = "Cyrus";
-        studentNames[5] = "Edwin";
-        studentNames[6] = "Eileen";
-        studentNames[7] = "Ella";
-        studentNames[8] = "Ellis";
-        studentNames[9] = "Nico";
-        studentNames[10] = "Peter";
-        studentNames[11] = "Pippa";
-        studentNames[12] = "Skye";
-        studentNames[13] = "Trevor";
-        studentNames[14] = "Tucker";
-        //StdOut.println(Arrays.toString(studentNames));
-        return studentNames;
-    }
-
     public static void main(String[] args) {
-        int x;
-        x = 5;
-        int y = 0;
-        y /= x;
-       System.out.println(y);
+        int[] intArray  =  new int[] {69,62,43,5,6,123,5323,5774};
+        int  temp  = 0;
+        //Print original array
+        System.out.println("Original array: ");
+        for (int i = 0; i <intArray.length; i++) {
+            System.out.print(intArray[i] + " ");
+        }
+        // Sort  Array in  Ascending Order
+        for(int i  = 0; i <intArray.length; i++) {
+            for (int  j = i+1;  j< intArray.length; j++) {
 
-    }
-
-    {
-
-
+                if(intArray[i] > intArray[j]) {
+                    temp  = intArray[i];
+                    intArray[i]    = intArray[j];
+                    intArray[j]  =  temp;
+                }
+            }
+        }
+        //  Print Sorted Array
+        System.out.println("Array  sorted  in ascending order ");
+        for(int i = 0; i<intArray.length; i++){
+            System.out.print(intArray[i] + " ");
+        }
     }
 }
