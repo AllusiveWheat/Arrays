f071af4e20abf117461c72aa0c3a3f569cb3007d
AllusiveWheat <37462003+AllusiveWheat@users.noreply.github.com>
2021-02-10 22:17:43 -0800

    Added Alphabetical Array

--- src/com/company/Gamer.java
+++ src/com/company/Gamer.java
@@ -1,9 +1,18 @@
 package com.company;
-
+import java.util.Arrays;
+import java.util.Collections;
 public class Gamer {
-
     public static void main(String[] args) {
+        //Print Original Array
+        String[] colors = {"Red" , "Black", "Blue" , "Yellow" , "Purple"};
+        System.out.println("Original Array " + Arrays.toString(colors));
 
+        //Print Array in Alphabetical Order
+        Arrays.sort(colors);
+        System.out.println("Sorted Array " + Arrays.toString(colors) );
 
+        //Print Array in Revered Alphabetical Order
+        Arrays.sort(colors , Collections.reverseOrder());
+        System.out.println("Reversed Array " + Arrays.toString(colors));
     }
 }
