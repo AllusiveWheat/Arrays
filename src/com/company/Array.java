2fbdef92d74d924833535332e272f8b59b5206c4
AllusiveWheat <37462003+AllusiveWheat@users.noreply.github.com>
2021-02-10 22:10:38 -0800

    Updates

--- src/com/company/Array.java
+++ src/com/company/Array.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class Array {
-    public  static void main(String[]args) {
-        String names[] = { "Ben","Alex" ,"Charlie" ,"Connor", "Cyrus","Eileen","Ella","Ellis","Peter","Edwin","Nico","Trevor","Tucker","Skye"};
+    public  static void main(String[] args) {
+        String[] names = { "Ben","Alex" ,"Charlie" ,"Connor", "Cyrus","Eileen","Ella","Ellis","Peter","Edwin","Nico","Trevor","Tucker","Skye"};
         System.out.printf("Original Array: \n%s\n\n", Arrays.toString(names));
 
         // Sort Array in ascending Order
