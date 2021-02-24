05a5a73ef10912e18f59235f838c9d5a5630705e
AllusiveWheat <37462003+AllusiveWheat@users.noreply.github.com>
2021-02-24 12:42:06 -0800

    Fixed

--- src/com/company/ReverseF.java
+++ src/com/company/ReverseF.java
@@ -2,19 +2,28 @@ package com.company;
 import java.util.*;
 public class ReverseF {
 
-    public static int[] reverseArray(int[] x) {
+
+    public static void main(String[] args) {
+        int[] temp = reverseArray();
+        System.out.println(Arrays.toString(temp));
+        //Print  in here the array from ^
+    }
+
+    private static int[] reverseArray() {
+        int[] x = new int[2];
+        x[0] =2142452423;
+        x[1]=56456464;
         for (int i = 0; i < x.length / 2; i++) {
             int temp = x[i];
             x[i] = x[x.length - 1 - i];
             x[x.length - 1 - i] = temp;
             //Or can you print in here?
+            Arrays.toString(x);
         }
         return x;
     }
-    public static void main(String[] args) {
-        //Print  in here the array from ^
-    }
 }
+
 
 
 
