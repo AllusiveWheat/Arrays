898ce901a1b5fbb907958e41fefc99c0563c9199
AllusiveWheat <37462003+AllusiveWheat@users.noreply.github.com>
2021-02-24 12:55:43 -0800

    Allowed Input

--- src/com/company/ReverseF.java
+++ src/com/company/ReverseF.java
@@ -2,15 +2,7 @@ package com.company;
 import java.util.*;
 public class ReverseF {
 
-
-    public static void main(String[] args) {
-        int[] temp = reverseArray();
-        System.out.println(Arrays.toString(temp));
-        //Print  in here the array from ^
-    }
-
-    private static int[] reverseArray() {
-        int[] x = new int[2];
+    private static int[] reverseArray(int[] x) {
         x[0] =2142452423;
         x[1]=56456464;
         for (int i = 0; i < x.length / 2; i++) {
