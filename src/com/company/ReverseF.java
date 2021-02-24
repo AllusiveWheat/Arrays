18b6d2668f1f9346f47c314fe7dcfcc5dc5a8bfb
Alex Free <41807606+HippomanX@users.noreply.github.com>
2021-02-24 12:49:05 -0800

    commit

--- src/com/company/ReverseF.java
+++ src/com/company/ReverseF.java
@@ -5,22 +5,24 @@ public class ReverseF {
 
     public static void main(String[] args) {
         int[] temp = reverseArray();
-        System.out.println(Arrays.toString(temp));
+        
         //Print  in here the array from ^
     }
 
     private static int[] reverseArray() {
-        int[] x = new int[2];
-        x[0] =2142452423;
-        x[1]=56456464;
-        for (int i = 0; i < x.length / 2; i++) {
-            int temp = x[i];
-            x[i] = x[x.length - 1 - i];
-            x[x.length - 1 - i] = temp;
+        int[] arr = new int[2];
+        arr[0] = 5;
+        arr[1] = 6;
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
             //Or can you print in here?
-            Arrays.toString(x);
+            
         }
-        return x;
+        //System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
+        return arr;
     }
 }
 
