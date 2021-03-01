d1426acee4009564593dd950ae065e6299de1552
AllusiveWheat <37462003+AllusiveWheat@users.noreply.github.com>
2021-03-01 14:47:13 -0800

    Added Input and Squared and Cube root

--- src/com/company/GetHyp.java
+++ src/com/company/GetHyp.java
@@ -5,33 +5,39 @@ public class GetHyp {
     public static double getHyp(double HypW, double HypH) {
         double hyp;
         hyp = Math.sqrt(Math.pow(HypW, 2) + Math.pow(HypH, 2));
-
-        return hyp;
-
+        return  hyp;
     }
 
     public static double getA(double Hyp, double HypB) {
-        double getA;
-         getA = Math.sqrt(Math.pow(Hyp, 2) - Math.pow(HypB, 2));
-
+        double getA = Math.sqrt(Math.pow(Hyp, 2) - Math.pow(HypB, 2));
         return getA;
     }
 
     public static double getB(double Hyp, double HypA) {
-        double getB;
-        getB = Math.sqrt(Math.pow(Hyp, 2) - Math.pow(HypA, 2));
-
+        double getB = Math.sqrt(Math.pow(Hyp, 2) - Math.pow(HypA, 2));
         return getB;
     }
 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter A ");
-        double a= sc.nextDouble();
-        System.out.print("Enter B ");
-        double b= sc.nextDouble();
-        double hyp = getHyp(a,b);
-        System.out.println("The hypotenuse is " + hyp);
 
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+          /*
+            System.out.print("Enter C: ");
+            double c = sc.nextDouble();
+            System.out.print("Enter B: ");
+            double b = sc.nextDouble();
+            double A = getA(c, b);
+            System.out.println("A is: " + A); */
+            System.out.print("Enter A Number:" );
+            int num = sc.nextInt();
+            System.out.println("Your number is "+ num);
+            int y= (int) Math.cbrt(num);
+            System.out.println("The Cube root of your Number is "+ y);
         }
+        catch(Exception e) {
+
+            System.out.println(e);
+        }
+    }
 }
